2435df0059e3d1fe7e76cd3a5c97d4ff14d927fd
ajaysisode <sisodiya.ajay85@gmail.com>
2022-08-17 18:26:15 -0700

    PBNP -1:Create A Project Structure

---
+++ PhoneBookAppProject/src/main/java/com/BikkadIt/PhoneBookApp/PhoneBookAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.PhoneBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookAppProjectApplication.class, args);
+	}
+
+}
---
+++ PhoneBookAppProject/src/test/java/com/BikkadIt/PhoneBookApp/PhoneBookAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.PhoneBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
